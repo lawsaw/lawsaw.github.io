4bf9a8d327ea1bb95d2644a3cac0c4a0df30e773
lawsaw <lawdrel@gmail.com>
2019-09-10 17:14:06 +0300

    fdsfs

---
+++ precache-manifest.e72842719a37aa6c2bf19bf811ed9c3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca21a17ec9c8a4f89dd2be38373449f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da31ccf9f4484856c410",
+    "url": "/static/css/main.5f295a77.chunk.css"
+  },
+  {
+    "revision": "951086e4c2ebd374e1ef",
+    "url": "/static/js/2.2249d68d.chunk.js"
+  },
+  {
+    "revision": "da31ccf9f4484856c410",
+    "url": "/static/js/main.1bbfd209.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
