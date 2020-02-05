dbf955cfe07209c04c2aaea511d04bf44a73e49b
lawsaw <lawdrel@gmail.com>
2020-02-05 16:56:04 +0200

    gamehub

---
+++ precache-manifest.8eb2a93a0ae0c09bb68cc9e49ff937ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eae944514648146794f3af2424e35482",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecf883795e8ccd71430d",
+    "url": "/static/css/main.bed29f1c.chunk.css"
+  },
+  {
+    "revision": "7066852dd4ae5be08d9b",
+    "url": "/static/js/2.7a639157.chunk.js"
+  },
+  {
+    "revision": "26fe61fb492d608298a80d8bc694d3ea",
+    "url": "/static/js/2.7a639157.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ecf883795e8ccd71430d",
+    "url": "/static/js/main.84aedf0e.chunk.js"
+  },
+  {
+    "revision": "6622d735c3a12c275ad0",
+    "url": "/static/js/runtime-main.7f48936f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6b0850a83dd7029a2a22e1ea091bd94b.js"
+  "/precache-manifest.8eb2a93a0ae0c09bb68cc9e49ff937ab.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
