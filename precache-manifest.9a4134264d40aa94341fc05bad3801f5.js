5faf1370e54ca3bd5f3c65692e6c406c91f60409
lawsaw <lawdrel@gmail.com>
2020-02-10 11:44:29 +0200

    prod

---
+++ precache-manifest.9a4134264d40aa94341fc05bad3801f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d73360e315938aaa1c8a4d3c1ab9b80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb3dfbc2150299d1b958",
+    "url": "/static/css/main.bed29f1c.chunk.css"
+  },
+  {
+    "revision": "9cf10ed6aa6f7f6a255a",
+    "url": "/static/js/2.080d0011.chunk.js"
+  },
+  {
+    "revision": "26fe61fb492d608298a80d8bc694d3ea",
+    "url": "/static/js/2.080d0011.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb3dfbc2150299d1b958",
+    "url": "/static/js/main.e71209db.chunk.js"
+  },
+  {
+    "revision": "6622d735c3a12c275ad0",
+    "url": "/static/js/runtime-main.7f48936f.js"
+  }
+]);
